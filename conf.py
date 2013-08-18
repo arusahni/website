71c56bc7cbb24da53299d661c52e3aba1778bb7a
Aru Sahni <arusahni@gmail.com>
2013-08-17 22:14:35 -0400

    Website now uses Analytics.

--- conf.py
+++ conf.py
@@ -451,7 +451,21 @@ RSS_TEASERS = True
 # EXTRA_HEAD_DATA = ""
 # Google analytics or whatever else you use. Added to the bottom of <body>
 # in the default template (base.tmpl).
-# ANALYTICS = ""
+ANALYTICS = """
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-38011141-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+"""
 
 # The possibility to extract metadata from the filename by using a
 # regular expression.
